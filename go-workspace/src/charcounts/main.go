456c391f05b2bea6a3a33a1f427410139bf5abd7
xvw <xavier.vdw@gmail.com>
2015-12-28 22:46:14 +0100

    Append Charcount

--- go-workspace/src/charcounts/main.go
+++ go-workspace/src/charcounts/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Analysis(str string) map[rune]int {
+	result := make(map[rune]int)
+	for _, rune := range str {
+		result[rune] += 1
+	}
+	return result
+}
+
+func Report(result map[rune]int) {
+	for rune, total := range result {
+		fmt.Printf("%c : %d \n", rune, total)
+	}
+}
 
 func main() {
-	fmt.Printf("Hello\n")
+
+	args := os.Args[1:]
+	str := strings.Join(args, " ")
+	fmt.Printf("In : [%s]\n", str)
+	Report(Analysis(str))
 }
